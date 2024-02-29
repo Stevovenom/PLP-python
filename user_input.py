10f4cfcd30937e5068cd59c3d5ae287c2c4c8c9b
stevovenom <stephenokoth2002@gmail.com>
2024-02-29 17:26:15 +0300

    User_input.py

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("whats your name?: ")
+age=int(input("whats your age?: "))
+location=input("Whats your location?: ")
+print(f'Hello {name}, you are {age} years old and live in {location}.')
